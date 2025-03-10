4715929d5b7bff67f49221c72e2638e912f1620f
Nawed@Personal <nawedakhtar1811@gmail.com>
2025-03-10 22:51:18 +0530

    lesson 3

--- lesson02/src/users/users.controller.ts
+++ lesson02/src/users/users.controller.ts
@@ -1,39 +1,49 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
-
+import { UsersService } from './users.service';
 @Controller('users')  // same as /users
 export class UsersController {
     
+    constructor (private readonly userService: UsersService){};
 
     @Get()  //  get/users  or users?role=value
     findall(@Query('role') role?:'INTERN' | 'ENGINEER'| 'ADMIN')
     {
-        return []
+        return this.userService.findall(role);
     }
 
     @Get(':id')  //get  /users/id
     findOne(@Param('id') id: string)
     {
-        return { id};
+        const user=this.userService.findOne(+id);   // +id will convert the string id into num id
+        
+        if(!user)
+        {
+            return { message:"user not found!"}
+        }
+        return user;
     }
 
 
     @Post()  //Post  /users/id
-    createUser(@Body() user:{})
+    createUser(@Body() user:{name:string,email:string, role:'INTERN' | 'ENGINEER'| 'ADMIN'})
     {
-        return user;
+        return this.userService.createUser(user);
     }
 
     @Patch(':id')  //Patch  /users/id
-    Update(@Param('id') id: string, @Body() userUpdate: {})
+    Update(@Param('id') id: string, @Body() userUpdate: {name?:string,email?:string, role?:'INTERN' | 'ENGINEER'| 'ADMIN'})
     {   
-        return { msg:"successfully updated user", id,...userUpdate};
+
+        return this.userService.Update(+id,userUpdate);
+        
     }
 
     @Delete(':id')  //Delete  /users/:id
     deleteuser(@Param('id') id: string)
     {   
-        return [];
+         this.userService.deleteuser(+id);
+         return {message:"user deleted successfully"}
     }
 
     
--- lesson02/src/users/users.service.ts
+++ lesson02/src/users/users.service.ts
@@ -1,4 +1,95 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+     private users= [
+        {
+            "id": 1,
+            "name": "Leanne Graham",
+            "email": "Sincere@april.biz",
+            "role": "INTERN",
+        },
+        {
+            "id": 2,
+            "name": "Ervin Howell",
+            "email": "Shanna@melissa.tv",
+            "role": "INTERN",
+        },
+        {
+            "id": 3,
+            "name": "Clementine Bauch",
+            "email": "Nathan@yesenia.net",
+            "role": "ENGINEER",
+        },
+        {
+            "id": 4,
+            "name": "Patricia Lebsack",
+            "email": "Julianne.OConner@kory.org",
+            "role": "ENGINEER",
+        },
+        {
+            "id": 5,
+            "name": "Chelsey Dietrich",
+            "email": "Lucio_Hettinger@annie.ca",
+            "role": "ADMIN",
+        }
+     ]
+
+
+    //   enum Role {                //could have also done with some fixes
+    //     Intern='INTERN',
+    //     Engineer="ENGINEER",
+    //     Admin='ADMIN'
+    //  }
+     findall(role?:'INTERN' | 'ENGINEER'| 'ADMIN')  //does strict type checking
+     {
+
+           if(role)
+           {
+              return this.users.filter((user)=>user.role===role);
+
+           }
+           return this.users;
+     }
+     findOne(id:number)
+     {
+          const user= this.users.find((user)=> user.id===id);
+
+            return user;
+          
+     }
+
+     createUser(user:{name:string,email:string, role:'INTERN' | 'ENGINEER'| 'ADMIN'})
+     {
+
+        const size= this.users.length;
+        const newUser= {
+            id:size+1,
+            ...user
+        }
+
+        this.users.push(newUser);
+        return newUser;
+     }
+
+     Update(id: number, updateUser:{name?:string,email?:string, role?:'INTERN' | 'ENGINEER'| 'ADMIN'})
+     {
+          this.users=this.users.map((user)=>{
+               
+               if(user.id===id)
+               {
+                  return {...user,...updateUser}
+               }
+               return user;
+          });
+          const user= this.findOne(id);
+          return user;
+     }
+
+     deleteuser(id:number)
+     {    
+          this.users= this.users.filter((user)=>user.id!==id);
+     }
+
+}
